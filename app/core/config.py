2dbca7d40cab15f1116364a14f624816d173555b
Arkady Schoenberg <shasoka@yandex.ru>
2025-04-15 19:27:21 +0700

    Now config reads envs from .env

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from pydantic import PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Run(BaseModel):
@@ -21,6 +21,13 @@ class Database(BaseModel):
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+        env_file=".env",
+    )
+
     run: Run = Run()
     api: ApiPrefix = ApiPrefix()
     db: Database
