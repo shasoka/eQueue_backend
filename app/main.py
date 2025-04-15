59f4d931c4fb99ae2ff144a578c9d3f32b9704f9
Arkady Schoenberg <shasoka@yandex.ru>
2025-04-15 19:00:44 +0700

    Added SQLAlchemy + asyncio

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app.include_router(
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
-        port=8000,
+        host=settings.run.host,
+        port=settings.run.port,
         reload=True,
     )
