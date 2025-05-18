5954bf37fb908c0002ffa5abf654ab8d72a527c7
Arkady Schoenberg <shasoka@yandex.ru>
2025-05-19 00:19:14 +0700

    Documented endpoints tagged with "Groups"

--- app/api/groups.py
+++ app/api/groups.py
@@ -16,6 +16,7 @@ router = APIRouter()
 @router.get(
     "/{id}",
     response_model=GroupRead,
+    summary="Получение информации о группе по ID",
     responses=generate_responses_for_swagger(
         codes=(
             status.HTTP_401_UNAUTHORIZED,
@@ -28,6 +29,10 @@ async def get_group_by_id(
     _: Annotated[User, Depends(get_current_user)],
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
 ) -> Group:
+    """
+    ### Эндпоинт для получения информации о группе по ID.
+    """
+
     return await _get_group_by_id(
         session=session,
         group_id=id,
@@ -38,6 +43,7 @@ async def get_group_by_id(
 @router.get(
     "",
     response_model=List[GroupRead],
+    summary="Получение полного списка групп",
     responses=generate_responses_for_swagger(
         codes=(status.HTTP_401_UNAUTHORIZED,),
     ),
@@ -46,4 +52,11 @@ async def get_groups_list(
     _: Annotated[User, Depends(get_current_user)],
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
 ) -> List[Group]:
+    """
+    ### Эндпоинт для получения полного списка групп.
+    \nВозвращает полный список групп, хранимых в базе данных.
+    \nСписок групп сформирован на этапе развертывания БД благодаря REST API еКурсов.
+    \nЭндпоинт для получения групп с сайта [`http://newtimetable.sfu-kras.ru`](http://newtimetable.sfu-kras.ru): ***https://edu.sfu-kras.ru/api/timetable/get_insts***.
+    """
+
     return await get_all_groups(session=session)
